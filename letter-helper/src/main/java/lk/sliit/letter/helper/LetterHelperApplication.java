f6a105a7abba3f2a1b7097ae13177be6d9a5ae30
Dasun Dilshan <164143926+kmddkuruppu@users.noreply.github.com>
2026-03-20 12:09:06 +0530

    Initial commit letter helper project

---
+++ letter-helper/src/main/java/lk/sliit/letter/helper/LetterHelperApplication.java
@@ -0,0 +1,13 @@
+package lk.sliit.letter.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetterHelperApplication.class, args);
+	}
+
+}
---
+++ letter-helper/src/test/java/lk/sliit/letter/helper/LetterHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sliit.letter.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
